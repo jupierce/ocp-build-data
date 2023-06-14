3fbbbd9d5393d8c518f3e037e89fcac5af88de16
Justin Pierce <jupierce@redhat.com>
2023-06-14 13:46:29 -0400

    Prevent nonlocal go compiler

--- target_go_wrapper.sh
+++ target_go_wrapper.sh
@@ -30,6 +30,8 @@ GO_COMPLIANCE_OPENSSL_ENABLED_INCLUDE=${GO_COMPLIANCE_OPENSSL_ENABLED_INCLUDE:-'
 
 if [[ -n "${OPENSHIFT_CI}" || "${__doozer_group}" == "openshift-"* ]]; then
   GO_COMPLIANCE_POLICY="${GO_COMPLIANCE_POLICY:-exempt_darwin,exempt_cross_compile}"
+  export GOTOOLCHAIN="local"
+  echoerr "Forcing GOTOOLCHAIN=${GOTOOLCHAIN}"
 else
   GO_COMPLIANCE_POLICY="exempt_all"
 fi
